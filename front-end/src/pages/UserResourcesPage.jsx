6a18f97c695298129033fc5a53e6c183f6b76d64
iam-agyenim <ra4150@nyu.edu>
2026-04-08 12:42:54 -0400

    feat: add profile icon to all profile circles

--- front-end/src/pages/UserResourcesPage.jsx
+++ front-end/src/pages/UserResourcesPage.jsx
@@ -42,7 +42,12 @@ const UserResourcesPage = () => {
               </div>
             )}
           </div>
-          <Link to="/profile" className="profileStub" aria-label="profile" />
+          <Link to="/profile" className="profileStub" aria-label="profile">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="#0a0d18">
+              <circle cx="12" cy="8" r="4" />
+              <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7" />
+            </svg>
+          </Link>
         </div>
       </header>
 
