93b6118d5862d1ce5df621e55e9973cba1fb8ff1
Mamadou Ndom <ndom15304@gmail.com>
2026-04-09 08:59:36 -0400

    finished courseRoutes and controllers

--- back-end/routes/courseRoutes.js
+++ back-end/routes/courseRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { getCourseById } = require("../controllers/courseController");
+const {
+  getCourseById,
+  saveCourseById,
+  getCourseResourcesById
+} = require("../controllers/courseController");
 
 const router = express.Router();
 
 // GET /courses/:id
 router.get("/:id", getCourseById);
+
+// POST /courses/:id/save
+router.post("/:id/save", saveCourseById);
+
+// GET /courses/:id/resources
+router.get("/:id/resources", getCourseResourcesById);
 
 module.exports = router;
